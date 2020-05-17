7cb8491abdefe7e130fb4c4765777ce55935f8a4
Alexander Swerdlow <aswerdlow1@gmail.com>
2020-05-16 22:35:35 -0700

    Update WebClientID

--- src/config/index.js
+++ src/config/index.js
@@ -4,5 +4,5 @@ export default {
     borderColor: 'rgb(233, 233, 233)',
   },
   webClientId:
-    '176502677559-30enu160h4aviebo60b3g6qkqg913rjs.apps.googleusercontent.com',
+    '176502677559-gi68rhr7f9rbii7gas2o1sj3ftvdeg53.apps.googleusercontent.com',
 };

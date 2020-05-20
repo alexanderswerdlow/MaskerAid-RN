f20e9aa8e1f6351e780c10b1f9c7d7da8ea9f967
Alexander Swerdlow <aswerdlow1@gmail.com>
2020-05-19 18:23:47 -0700

    Added Image Picker
    Changed Info.plist and AndroidManifest to use ios/android native image selection. Not yet able to display the photo.

--- src/screens/NewPost.js
+++ src/screens/NewPost.js
@@ -1,11 +1,39 @@
-import React, {Component} from 'react';
-import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, TouchableOpacity, Image} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+
+const options = {
+  title: 'Select Avatar',
+  customButtons: [{name: 'fb', title: 'Choose Photo from Facebook'}],
+  storageOptions: {
+    skipBackup: true,
+    path: 'images',
+  },
+};
 
 function NewPost({navigation}) {
+  const [avatarSource, setAvatarSource] = useState('');
+
+  async function _loadImage() {
+    ImagePicker.showImagePicker(options, (response) => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        setAvatarSource(response.uri);
+      }
+    });
+  }
+
   return (
-    <TouchableOpacity style={styles.container}>
-      <Text>NEW POST</Text>
-    </TouchableOpacity>
+    <>
+      <TouchableOpacity style={styles.container} onPress={() => _loadImage()}>
+        <Text>NEW POST</Text>
+      </TouchableOpacity>
+    </>
   );
 }
 

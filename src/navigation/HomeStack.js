aa647489bada4bbac52db1e82d216aae262df464
Alexander Swerdlow <aswerdlow1@gmail.com>
2020-05-19 18:07:28 -0700

    Added Post Page to nav bar

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/Home';
 import ProfileScreen from '../screens/Profile';
+import NewPostScreen from '../screens/NewPost';
+
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
@@ -17,6 +19,8 @@ export default function App() {
             iconName = 'ios-home';
           } else if (route.name === 'Profile') {
             iconName = 'ios-person';
+          } else if (route.name === 'Post') {
+            iconName = 'ios-clipboard';
           }
 
           return <Ionicons name={iconName} size={size} color={color} />;
@@ -27,6 +31,7 @@ export default function App() {
         inactiveTintColor: 'gray',
       }}>
       <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Post" component={NewPostScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
--- src/screens/NewPost.js
+++ src/screens/NewPost.js
@@ -1,27 +1,22 @@
-import React, { Component } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import React, {Component} from 'react';
+import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
 
-class NewPost extends Component {
-    login() {
-        this.props.navigation.navigate("main");
-    }
-
-    render() {
-        return (
-            <TouchableOpacity
-                style={{
-                    height: 100 + "%",
-                    width: 100 + "%",
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}
-            >
-
-                <Text>NEW POST</Text>
-            </TouchableOpacity>
-        );
-    }
+function NewPost({navigation}) {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <Text>NEW POST</Text>
+    </TouchableOpacity>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: 100 + '%',
+    width: 100 + '%',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default NewPost;

6fc7997fb637cc9eb6fee2fb0d472e6100ea8566
Kaivalya Tota <ktot@ucla.edu>
2020-05-20 23:52:10 -0400

    update profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,17 +1,55 @@
-import React, {useContext} from 'react';
-import {View, Text, StyleSheet, Image, Dimensions, Button} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {View, Text, StyleSheet, Image, Dimensions, TouchableOpacity} from 'react-native';
 import {AuthContext} from '../navigation/AuthProvider';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+export function handlePress() {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
 
 export default function Profile({navigation}) {
+  const screenWidth = Dimensions.get('window').width;
   const {user, logout} = useContext(AuthContext);
   return (
     <View>
-      <Image
-        style={styles.userPic}
-        source={{uri: user.photoURL}}
-        resizeMode="stretch"
-      />
-      <Button title="Logout" onPress={() => logout()} />
+      <View>
+        <View>
+          <Image style={styles.userPic}
+            source={{uri: user.photoURL}}
+            resizeMode="stretch"
+          />
+          <Text style={styles.userName}>
+            {user.displayName}
+          </Text>
+        </View>
+        <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+          <View style={{alignItems: 'center'}}>
+            <Text style={styles.userStatus}>20</Text>
+            <Text>Posts</Text>
+          </View>
+          <View style={{alignItems: 'center'}}>
+            <Text style={styles.userStatus}>20</Text>
+            <Text>Following</Text>
+          </View>
+          <View style={{alignItems: 'center'}}>
+            <Text style={styles.userStatus}>20</Text>
+            <Text>Followers</Text>
+          </View>
+        </View>
+      </View>
+
+
+      <View>
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity onPress={() => handlePress()}>
+            <Ionicons name={'apps-outline'} size={27} style={{padding: 5}}/>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
@@ -19,10 +57,28 @@ export default function Profile({navigation}) {
 const styles = StyleSheet.create({
   userPic: {
     alignSelf: 'center',
-    width: 100,
-    height: 100,
+    width: 125,
+    height: 125,
     borderRadius: 75,
+    marginTop: 30,
+    borderWidth: 1,
+    borderColor: 'black',
   },
 
-  userName: {},
+  userName: {
+    alignSelf: 'center',
+    fontSize: 25,
+    marginTop: 15,
+    marginBottom: 20,
+  },
+
+  userStatus: {
+    alignSelf: 'center',
+    fontSize: 20,
+  },
+
+  feed: {
+    alignSelf: 'center',
+    fontSize: 40,
+  }
 });
